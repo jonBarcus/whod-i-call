decfb252f2e1156adc3d77ec1485543d21823971
jonBarcus <barcus.j@gmail.com>
2014-09-13 21:06:28 -0400

    updated routes and entries_controller

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -1,4 +1,37 @@
 class EntriesController < ApplicationController
 
+  def index
+    @entries = Entry.all
+  end
+
+  def new
+    @communication_types = CommunicationType.all
+  end
+
+  def create
+    @entry = Entry.new(entry_params)
+
+    if @entry.save
+      redirect_to("/")
+    else
+      render("/entries/new")
+    end
+  end
+
+  def show
+    @entry = Entry.all
+  end
+
+  private
+
+  def entry_params
+    params.require(:entry).permit(
+      :date,
+      :contact_name,
+      :organization_name,
+      :notes,
+      :communication_type_id
+      )
+  end
 
 end
