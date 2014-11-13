4e4453fcba991a62774eafcb6d04b4a135d7cd69
jonBarcus <barcus.j@gmail.com>
2014-11-13 10:19:28 -0500

    ran rake db:migrate for the users table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140912195440) do
+ActiveRecord::Schema.define(version: 20141113145414) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,6 +30,12 @@ ActiveRecord::Schema.define(version: 20140912195440) do
     t.integer  "communication_type_id"
     t.datetime "created_at"
     t.datetime "updated_at"
+  end
+
+  create_table "users", force: true do |t|
+    t.string "email"
+    t.string "name"
+    t.string "password_digest"
   end
 
 end
