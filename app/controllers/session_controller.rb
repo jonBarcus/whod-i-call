557d05dc3c26a45e646c73a0abb9ff39ae5981bf
jonBarcus <barcus.j@gmail.com>
2014-11-17 16:34:45 -0500

    completed SessionController.

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -1,6 +1,21 @@
 class SessionController < ApplicationController
 
+  def create
+    user = User.find_by(email: params[:email])
+
+    if user && user.authenticate(params[:password])
+
+      session[:user_id] = user.id
+      redirect_to("/")
+    else
+      render(:new)
+    end
+  end
 
 
+  def destroy
+    session[:user_id] = nil
+    redirect_to(root_path)
+  end
 
 end
