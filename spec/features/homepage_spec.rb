7a328078840183e91411f708e9f32af8f6240aea
jonBarcus <barcus.j@gmail.com>
2015-02-18 20:16:52 -0500

    Continuing to write Capybara tests. Wrote test for Log Out.  Brainstorming what other tests to write.  Will continue to work.  Testing is fun

--- spec/features/homepage_spec.rb
+++ spec/features/homepage_spec.rb
@@ -3,16 +3,35 @@ require 'spec_helper'
 
 describe "the sign-in process", :type => feature do
   before :each do
-    User.create(:email => 'test@test.com', :password => 'test')
+    User.create(:email => 'test@test.com', :password => 'testtest')
   end
 
   it "signs me in" do
     visit '/session/new'
     within("body") do
       fill_in 'email', :with => 'test@test.com'
-      fill_in 'password', :with => 'test'
+      fill_in 'password', :with => 'testtest'
     end
     click_button('Login')
+    expect(page).to have_content 'New Entry'
+  end
+
+end
+
+describe "the log-out process", :type => feature do
+  before :each do
+    User.create(:email => 'test@test.com', :password => 'testtest')
+  end
+
+  it "logs me out" do
+    visit '/session/new'
+    within("body") do
+      fill_in 'email', :with => 'test@test.com'
+      fill_in 'password', :with => 'testtest'
+    end
+    click_button('Login')
+    click_link('Log Out')
+    expect(page).to have_content 'Sign In'
   end
 
 
