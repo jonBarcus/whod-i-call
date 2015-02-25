f65d110ca1b18900bbd6792f4b027cd09eb16df3
jonBarcus <barcus.j@gmail.com>
2015-02-25 17:10:56 -0500

    Continuing work on Entries tests.

--- spec/features/entries_spec.rb
+++ spec/features/entries_spec.rb
@@ -1,17 +1,4 @@
 describe "Entry creation process", :type => feature do
-  # before :each do
-  #   User.create(:email => 'test@test.com', :password => 'testtest')
-  # end
-
-  # it "signs me in" do
-  #   visit '/session/new'
-  #   within("body") do
-  #     fill_in 'email', :with => 'test@test.com'
-  #     fill_in 'password', :with => 'testtest'
-  #   end
-  #   click_button('Login')
-  #   expect(page).to have_content 'New Entry'
-  # end
   before :each do
     User.create(:email => 'test@test.com', :password => 'testtest')
     visit '/session/new'
@@ -25,6 +12,7 @@ describe "Entry creation process", :type => feature do
 
   it "creates an entry" do
     click_link('New Entry')
+    expect(page).to have_content 'Create a New Entry'
   end
 
 end
