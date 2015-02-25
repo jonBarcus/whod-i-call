8256bec6ab9b01790fd66a1da87aadcaf3daadcd
jonBarcus <barcus.j@gmail.com>
2015-02-25 15:38:03 -0500

    Continuing to write tests using Capybara. Will continue to flesh out the various tests for Entries with an aim to push this to Heroku in the next couple of days.

--- spec/features/entries_spec.rb
+++ spec/features/entries_spec.rb
@@ -12,12 +12,19 @@ describe "Entry creation process", :type => feature do
   #   click_button('Login')
   #   expect(page).to have_content 'New Entry'
   # end
+  before :each do
+    User.create(:email => 'test@test.com', :password => 'testtest')
+    visit '/session/new'
+    within("body") do
+      fill_in 'email', :with => 'test@test.com'
+      fill_in 'password', :with => 'testtest'
+    end
+    click_button('Login')
+    expect(page).to have_content 'New Entry'
+  end
 
   it "creates an entry" do
-    visit '/entires/new'
-    within("body") do
-
-    end
+    click_link('New Entry')
   end
 
 end
